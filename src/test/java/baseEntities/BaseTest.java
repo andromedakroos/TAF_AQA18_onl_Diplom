875ee800daabe1a6e7c386a9fd21eae64cf49dd5
Andrew Moroz <andromedakroos@gmail.com>
2023-02-26 19:21:49 +0100

    allure

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -5,13 +5,16 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import configuration.ReadProperties;
 import io.qameta.allure.selenide.AllureSelenide;
 import models.User;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import steps.LoginStep;
+import utils.InvokedListener;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
-
+@Listeners(InvokedListener.class)
 public class BaseTest {
     protected LoginStep loginStep;
     protected User userSuccessful;
@@ -19,9 +22,9 @@ public class BaseTest {
     protected User userWithLongPassword;
     protected User userWithShortPassword;
     @BeforeSuite
-    public void setUp() {
+    public void setUp(ITestContext iTestContext) {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
+        iTestContext.setAttribute("driver", driver);
         Configuration.browser = ReadProperties.browserName();
         Configuration.baseUrl = ReadProperties.getUrl();
         Configuration.timeout = 15000;
