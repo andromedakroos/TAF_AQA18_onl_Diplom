147a66be48a78cf870a0d7fd631b96c300ce5111
Andrew Moroz <andromedakroos@gmail.com>
2023-02-26 19:37:17 +0100

    allure done

--- src/test/java/tests/GUI/negative/InputIncorrectDataTest.java
+++ src/test/java/tests/GUI/negative/InputIncorrectDataTest.java
@@ -10,6 +10,6 @@ import static com.codeborne.selenide.Condition.visible;
 public class InputIncorrectDataTest extends BaseTest {
     @Test(description = "Тест на использование некорректных данных")
     public void InputIncorrectDataTest(){
-      loginStep.loginIncorrect(userIncorrect).shouldHave(text("Hello"));
+      loginStep.loginIncorrect(userIncorrect).shouldHave(text("Email/Login or Password is incorrect. Please try again."));
     }
 }
